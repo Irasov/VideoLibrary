706ff09b3f8cfe18810db18a6d23d61ab068afb2
Irasov <srnan@yandex.ru>
2015-07-16 15:00:52 +0600

    DAO

--- src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
+++ src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
@@ -7,7 +7,12 @@ import com.epam.irasov.videolibrary.entity.Movie;
 public class Runner {
     public static void main(String[] args) {
         DaoFactory daoFactory = DaoFactory.getInstance();
+        daoFactory.beginTx();
         MovieDao movieDao = daoFactory.newMovieDao();
-        Movie movie = movieDao.findById(1l);
+
+        Movie movie = movieDao.findById(1L);
+        System.out.printf(movie.toString());
+        daoFactory.endTx();
+        daoFactory.close();
     }
 }

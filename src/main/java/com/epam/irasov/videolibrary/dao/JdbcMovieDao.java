426f94ffc2a387ae8e9a9c1b53d72a27f8861704
Irasov <srnan@yandex.ru>
2015-07-16 13:20:01 +0600

    findbyid

--- src/main/java/com/epam/irasov/videolibrary/dao/JdbcMovieDao.java
+++ src/main/java/com/epam/irasov/videolibrary/dao/JdbcMovieDao.java
@@ -3,18 +3,36 @@ package com.epam.irasov.videolibrary.dao;
 import com.epam.irasov.videolibrary.entity.Movie;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
-public class JdbcMovieDao implements MovieDao{
+public class JdbcMovieDao implements MovieDao {
+    private final static String FIND_BY_ID="SELECT ID_MOVIE, NAME, COUNTRY, DATE FROM MOVIE WHERE ID_MOVIE = ?";
 
     private final Connection connection;
 
-    public JdbcMovieDao(Connection connection){
+    public JdbcMovieDao(Connection connection) {
         this.connection = connection;
     }
 
 
     public Movie findById(Long id) {
-        return null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean found =resultSet.next();
+            if(!found) return null;
+            Movie movie = new Movie();
+            movie.setId(resultSet.getLong("ID"));
+            movie.setName(resultSet.getString("NAME"));
+            movie.setRelease(resultSet.getDate("DATE"));
+            movie.setCountry(resultSet.getString("COUNTRY"));
+            return movie;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
     }
 
     public void update(Movie movie) {

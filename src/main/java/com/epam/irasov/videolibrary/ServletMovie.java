c1370adf2a53fedb5ef7be3ff49da7f66504ff21
Irasov <srnan@yandex.ru>
2015-07-16 18:55:51 +0600

    servlet

---
+++ src/main/java/com/epam/irasov/videolibrary/ServletMovie.java
@@ -0,0 +1,14 @@
+package com.epam.irasov.videolibrary;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "ServletMovie")
+public class ServletMovie extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}

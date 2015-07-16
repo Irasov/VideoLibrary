04a174d80e3dc635ab86201e0a8b883aa4be44cb
Irasov <srnan@yandex.ru>
2015-07-16 16:53:19 +0600

    LOGGER

--- src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
+++ src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
@@ -3,15 +3,18 @@ package com.epam.irasov.videolibrary.runner;
 import com.epam.irasov.videolibrary.dao.DaoFactory;
 import com.epam.irasov.videolibrary.dao.MovieDao;
 import com.epam.irasov.videolibrary.entity.Movie;
+import org.apache.log4j.Logger;
 
 public class Runner {
+    private static Logger LOGGER = Logger.getLogger(Runner.class);
+
     public static void main(String[] args) {
         DaoFactory daoFactory = DaoFactory.getInstance();
         daoFactory.beginTx();
         MovieDao movieDao = daoFactory.newMovieDao();
 
         Movie movie = movieDao.findById(1L);
-        System.out.printf(movie.toString());
+        LOGGER.info(movie.toString());
         daoFactory.endTx();
         daoFactory.close();
     }

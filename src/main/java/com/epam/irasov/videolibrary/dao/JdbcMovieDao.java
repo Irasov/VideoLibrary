97f6f9417ff0ed08788b5016fd33eaa2f15690f4
Irasov <srnan@yandex.ru>
2015-07-16 15:33:23 +0600

    DAO

--- src/main/java/com/epam/irasov/videolibrary/dao/JdbcMovieDao.java
+++ src/main/java/com/epam/irasov/videolibrary/dao/JdbcMovieDao.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class JdbcMovieDao implements MovieDao {
-    private final static String FIND_BY_ID="SELECT ID_MOVIE, NAME, COUNTRY, DATE FROM MOVIE WHERE ID_MOVIE = ?";
+    private final static String FIND_BY_ID="SELECT ID, NAME, COUNTRY, DATE FROM MOVIE WHERE ID = ?";
 
     private final Connection connection;
 
@@ -27,10 +27,11 @@ public class JdbcMovieDao implements MovieDao {
             boolean found =resultSet.next();
             if(!found) return null;
             Movie movie = new Movie();
-            movie.setId(resultSet.getLong("ID_MOVIE"));
-            movie.setName(resultSet.getString("NAME"));
-            movie.setRelease(resultSet.getDate("DATE"));
-            movie.setCountry(resultSet.getString("COUNTRY"));
+            movie.setId(resultSet.getLong("id"));
+            movie.setName(resultSet.getString("name"));
+            movie.setRelease(resultSet.getDate("date"));
+            movie.setCountry(resultSet.getString("country"));
+
             return movie;
         } catch (SQLException e) {
             throw new DaoException(e);

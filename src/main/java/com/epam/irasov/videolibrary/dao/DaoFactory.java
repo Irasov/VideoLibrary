8ec5df57c66c9792823bc04eb9d68e8b7123e78e
Irasov <srnan@yandex.ru>
2015-07-19 15:52:37 +0600

    reName

--- src/main/java/com/epam/irasov/videolibrary/dao/DaoFactory.java
+++ src/main/java/com/epam/irasov/videolibrary/dao/DaoFactory.java
@@ -1,18 +1,14 @@
 package com.epam.irasov.videolibrary.dao;
 
-import com.epam.irasov.videolibrary.bd.ConnPool;
-import com.epam.irasov.videolibrary.bd.ListConnPool;
+import com.epam.irasov.videolibrary.bd.ConnectionPool;
 
 import java.sql.Connection;
 
 public abstract  class DaoFactory {
 
-   // private static final ConnPool pool = ConnPool.getInstance();
-
-   // private static final ListConnPool pool; = ListConnPool.initialization();
+    private static final ConnectionPool pool = ConnectionPool.getInstance();
 
     public static DaoFactory getInstance(){
-
         Connection connection = pool.takeConnection();
         return new JdbcDaoFactory(connection);
     }

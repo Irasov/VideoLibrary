8124e2963adb02f6177f5165158cf938848468bc
Irasov <srnan@yandex.ru>
2015-07-21 19:53:35 +0600

    TESTS

--- src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
+++ src/main/java/com/epam/irasov/videolibrary/runner/Runner.java
@@ -1,21 +1,6 @@
 package com.epam.irasov.videolibrary.runner;
+public class Runner  {
+    public static void main(String[] args){
 
-import com.epam.irasov.videolibrary.dao.DaoFactory;
-import com.epam.irasov.videolibrary.dao.MovieDao;
-import com.epam.irasov.videolibrary.entity.Movie;
-import org.apache.log4j.Logger;
-
-public class Runner {
-    private static Logger LOGGER = Logger.getLogger(Runner.class);
-
-    public static void main(String[] args) {
-        DaoFactory daoFactory = DaoFactory.getInstance();
-        daoFactory.beginTx();
-        MovieDao movieDao = daoFactory.newMovieDao();
-
-        Movie movie = movieDao.findById(1L);
-        LOGGER.info(movie.toString());
-        daoFactory.endTx();
-        daoFactory.close();
     }
 }

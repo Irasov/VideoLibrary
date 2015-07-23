b3c5a208d5b824a5276fc84d87ef41dae1407a8c
Irasov <srnan@yandex.ru>
2015-07-23 12:27:19 +0600

    find member by count movie

--- src/main/java/com/epam/irasov/videolibrary/dao/MovieDao.java
+++ src/main/java/com/epam/irasov/videolibrary/dao/MovieDao.java
@@ -2,13 +2,16 @@ package com.epam.irasov.videolibrary.dao;
 
 import com.epam.irasov.videolibrary.entity.Movie;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MovieDao {
     Movie findById(Long id);
     void update (Movie movie);
     Movie save (Movie movie);
     Movie merge (Movie movie);
-    Movie insert(Movie movie);
+    List<Movie.Member> selectMember(int countMovie);
     Movie.Member insertMember(Movie.Member member);
     boolean remove(Movie movie);
-    void removeByID(Long id);
+    void removeByDate(LocalDate date);
 }
